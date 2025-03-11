4f0d8fa1d1d29e37ef0cf132ce6925cd4b903003
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-11 11:26:06 +0000

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def home(request):
+  products = Products.object.all()
+  return render(request,'home.html',{'products':products})
