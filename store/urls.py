4dd49bc50bb51c569212f45504eb6e5db3cf8a7e
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-11 11:30:56 +0000

    Create urls.py

---
+++ store/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),  # Homepage for the store
+    path("category/<slug:slug>/", views.category_products, name="category_products"),
+]
