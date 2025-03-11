aad342d8232fce05d75213673675799524890801
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-11 12:55:11 +0000

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),  # Homepage for the store
     path("category/<slug:slug>/", views.category_products, name="category_products"),
+    path('register/',views.register,name='register'),
 ]
