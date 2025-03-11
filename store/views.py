13eab3533635e5bc930d9535a525aa5c15ee734f
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-11 12:54:00 +0000

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
   products = Products.object.all()
   return render(request, 'store/store.html', {'products': products})
+
+#Create User Authentication
+def register(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('login')
+    else:
+      form = UserCreationForm()
+    return render(request,'store/register.html',{'form':form})
