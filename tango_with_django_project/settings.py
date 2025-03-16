bdfd762dd12b7e7bfa41cf8cb6b299ab3a76f818
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-16 19:48:20 +0000

    Updated

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i579@7+k&8*_jt5#4e=)hjhbvancr!)pi7uns&1elnt1a5q+q9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tfa_store',
 ]
 
 MIDDLEWARE = [
