581756573f7bd2ad9a80212d49bc0d5281d137d9
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-16 14:01:55 +0000

    updated

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
-
+from django.contrib import tfa_store
+from app.models import *
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Cart)
+admin.site.register(stateModel)
+admin.site.register(AddressModel)
+admin.site.register(placeOrder)
+admin.site.register(sub_placeorder)
+admin.site.register(Contact)
+
+admin.site.site_header = 'The Fashion Atelier Admin'
