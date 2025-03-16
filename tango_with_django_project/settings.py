2f77063349103aa8ce68f63d36e6535ef92cd183
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-16 23:06:29 +0000

    Update settings.py

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i579@7+k&8*_jt5#4e=)hjhbvancr!)pi7uns&1elnt1a5q+q9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.135.6.2']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.135.6.2','0.0.0.0']
 
 
 # Application definition
