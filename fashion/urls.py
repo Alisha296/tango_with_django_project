28f48e9c1baddd0fff36c16e78accd45dffb193b
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-24 13:08:01 +0000

    update

--- fashion/urls.py
+++ fashion/urls.py
@@ -1,6 +1,7 @@
 # fashion/urls.py
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
