ec0bcfb31e3396b7cedfaeb6cc6e0f6bee625ce5
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-24 14:43:33 +0000

    Update urls.py

--- fashion/urls.py
+++ fashion/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     url(r'^reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     url(r'^profile/',views.profile, name='profile'),
+    url(r'^reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
