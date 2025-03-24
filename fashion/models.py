a85468e458ac877132a8668260c120c0f8b46ac3
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-24 01:21:46 +0000

    updated

--- fashion/models.py
+++ fashion/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+# fashion_app/models.py
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('men', 'Men'),
+        ('women', 'Women'),
+        ('kids', 'Kids'),
+    ]
+
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
